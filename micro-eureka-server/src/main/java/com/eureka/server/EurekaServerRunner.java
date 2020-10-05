38b1eb99dd52b9673783c41a6342182ace452c11
Ahasan JAVA <ahasan@atilimited.net>
2020-10-05 17:34:06 +0600

    update spring security configure in zuul

--- micro-eureka-server/src/main/java/com/eureka/server/EurekaServerRunner.java
+++ micro-eureka-server/src/main/java/com/eureka/server/EurekaServerRunner.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer // Enable eureka server
-
 public class EurekaServerRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerRunner.class, args);
