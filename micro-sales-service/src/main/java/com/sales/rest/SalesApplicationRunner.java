b0dd307285ffeb076a06e3ba88ed12226263bedd
Ahasan Habib <habibsumoncse@gmail.com>
2020-10-25 10:56:40 +0600

    remoce security

--- micro-sales-service/src/main/java/com/sales/rest/SalesApplicationRunner.java
+++ micro-sales-service/src/main/java/com/sales/rest/SalesApplicationRunner.java
@@ -3,11 +3,9 @@ package com.sales.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableResourceServer
 public class SalesApplicationRunner {
 
 	public static void main(String[] args) {
